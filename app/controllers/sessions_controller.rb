456b2086b4a74b5d99e9d2b47f0c13a7a85caae6
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-26 17:55:42 -0600

    Add pretty flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,16 @@ class SessionsController < ApplicationController
   def create
     if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
+      flash[:success] = "You are now logged in!"
+    else
+      flash[:danger] = "Something went wrong!"
     end
     redirect_to root_path
   end
 
   def destroy
     session.clear
+    flash[:success] = "You are now logged out!"
     redirect_to root_path
   end
 end
