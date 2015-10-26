7aa7676e0b6a917ee6e5963ffa151e88b43e98d4
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-26 15:57:49 -0600

    Setup test helper to stub OmniAuth

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,23 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      provider: "twitter",
+      extra: {
+        raw_info: {
+          user_id: 1111,
+          name: "Jeff",
+          screen_name: "jef",
+        }
+      },
+      credentials: {
+        token: "whatever",
+        secret: "whateversecret"
+      }
+    })
+  end
 end
