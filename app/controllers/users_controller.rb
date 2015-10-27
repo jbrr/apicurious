b981739c34e5b93fd245cabb771ee696bb2a1d39
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-26 20:53:09 -0600

    Ensure that users who aren't logged in can't view profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
+    unless current_user
+      flash[:notice] = "You must log in to see your profile"
+      redirect_to root_path
+    end
   end
 end
--- test/integration/user_logs_in_with_twitter_test.rb
+++ test/integration/user_logs_in_with_twitter_test.rb
@@ -29,4 +29,11 @@ class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Nude dude with a rude 'tude")
     assert page.has_link?("Logout")
   end
+
+  test "can't view profile page unless logged in" do
+    visit "/profile"
+    assert_equal "/", current_path
+    assert page.has_content?("You must log in to see your profile")
+    assert page.has_link?("Login")
+  end
 end
