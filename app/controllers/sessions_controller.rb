ba771776f664d1cbdfdea89dbff77983276b80b3
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-29 10:18:53 -0600

    Remove flash messages, I cannot figure out how to test favorites for the life of me, and I don't have time to figure it out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,14 @@ class SessionsController < ApplicationController
   def create
     if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
-      flash[:success] = "You are now logged in!"
       redirect_to root_path
     else
-      flash[:notice] = "Something went wrong!"
       redirect_to root_path
     end
   end
 
   def destroy
     session.clear
-    flash[:success] = "You are now logged out!"
     redirect_to root_path
   end
 end
